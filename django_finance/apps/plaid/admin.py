ddc5edde97e778d3e8f714fee4223ce82b74b8a2
earthcomfy <hanagetbel93@gmail.com>
2024-09-09 10:18:37 +0300

    feat: create account and transaction models

--- django_finance/apps/plaid/admin.py
+++ django_finance/apps/plaid/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 
-from django_finance.apps.plaid.models import Item, PlaidLinkEvent
+from django_finance.apps.plaid.models import Account, Item, PlaidLinkEvent, Transaction
 
-admin.site.register(Item)
 admin.site.register(PlaidLinkEvent)
+
+
+class PlaidTransactionInline(admin.TabularInline):
+    model = Transaction
+    extra = 1
+
+
+class AccountInline(admin.TabularInline):
+    model = Account
+    extra = 1
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [AccountInline]
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [PlaidTransactionInline]
+    list_display = ("account_id", "name", "account_type", "item")
+    search_fields = ("account_id", "name", "account_type")
+
+
+@admin.register(Transaction)
+class PlaidTransactionAdmin(admin.ModelAdmin):
+    list_display = ("transaction_id", "amount", "date", "account")
+    search_fields = ("transaction_id", "name", "merchant_name")
+    list_filter = ("date", "pending", "confidence_level")
--- django_finance/apps/plaid/models.py
+++ django_finance/apps/plaid/models.py
@@ -94,3 +94,213 @@ class PlaidLinkEvent(BaseModel):
 
     def __str__(self):
         return f"LinkEvent: user_id={self.user_id}, type={self.event_type}"
+
+
+class Account(BaseModel):
+    """
+    Used to store the accounts associated with each item.
+    """
+
+    class ACCOUNT_TYPE_CHOICES(models.TextChoices):
+        INVESTMENT = "investment", _("Investment")
+        CREDIT = "credit", _("Credit")
+        DEPOSITORY = "depository", _("Depository")
+        LOAN = "loan", _("Loan")
+        BROKERAGE = "brokerage", _("Brokerage")
+        OTHER = "other", _("Other")
+
+    item = models.ForeignKey(Item, related_name="accounts", on_delete=models.CASCADE)
+    account_id = models.CharField(
+        unique=True,
+        max_length=255,
+        help_text=_("Plaid’s unique identifier for the account."),
+    )
+    name = models.CharField(
+        max_length=250,
+        help_text=_("The name of the account, either assigned by the user or by the financial institution itself."),
+    )
+    mask = models.CharField(
+        max_length=4,
+        blank=True,
+        help_text=_("The last 2-4 alphanumeric characters of an account's official account number."),
+    )
+    official_name = models.TextField(
+        blank=True,
+        help_text=_("The official name of the account as given by the financial institution."),
+    )
+    available_balance = models.DecimalField(
+        max_digits=65,
+        decimal_places=30,
+        blank=True,
+        null=True,
+        help_text=_(
+            "The amount of funds available to be withdrawn from the account, as determined by the financial institution."
+        ),
+    )
+    current_balance = models.DecimalField(
+        max_digits=65,
+        decimal_places=30,
+        blank=True,
+        null=True,
+        help_text=_("The total amount of funds in or owed by the account."),
+    )
+    limit = models.DecimalField(
+        max_digits=65,
+        decimal_places=30,
+        blank=True,
+        null=True,
+        help_text=_("For credit-type accounts, this represents the credit limit."),
+    )
+    iso_currency_code = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text=_("The ISO-4217 currency code of the balance."),
+    )
+    unofficial_currency_code = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text=_("The unofficial currency code associated with the balance."),
+    )
+    account_type = models.CharField(
+        max_length=20,
+        choices=ACCOUNT_TYPE_CHOICES,
+        help_text=_("Account types. Possible values: investment, credit, depository, loan, brokerage, other"),
+    )
+    account_subtype = models.CharField(max_length=250, blank=True, help_text=_("Account subtype"))
+
+    def __str__(self):
+        return f"Account {self.account_id}, Item {self.item}"
+
+
+class Transaction(BaseModel):
+    """
+    Used to store the transactions associated with each account.
+    """
+
+    class TRANSACTION_CATEGORY_CONFIDENCE_LEVEL(models.TextChoices):
+        UNKNOWN = "unknown", _("Unknown")
+        LOW = "low", _("Low")
+        MEDIUM = "medium", _("Medium")
+        HIGH = "high", _("High")
+        VERY_HIGH = "very_high", _("Very High")
+
+    account = models.ForeignKey(Account, related_name="transactions", on_delete=models.CASCADE)
+    transaction_id = models.CharField(
+        unique=True,
+        max_length=255,
+        help_text=_(
+            "The unique ID of the transaction. Like all Plaid identifiers, the transaction_id is case sensitive."
+        ),
+    )
+    amount = models.DecimalField(
+        max_digits=65,
+        decimal_places=30,
+        blank=True,
+        null=True,
+        help_text=_(
+            "The settled value of the transaction, denominated in the transactions's currency, as stated in iso_currency_code or unofficial_currency_code."
+        ),
+    )
+    iso_currency_code = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text=_("The ISO-4217 currency code of the balance."),
+    )
+    unofficial_currency_code = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text=_("The unofficial currency code associated with the balance."),
+    )
+    check_number = models.TextField(
+        blank=True,
+        help_text=_("The check number of the transaction. This field is only populated for check transactions."),
+    )
+    location = models.JSONField(help_text=_("A representation of where a transaction took place."))
+    name = models.TextField(blank=True, help_text=_("The merchant name or transaction description."))
+    merchant_name = models.TextField(
+        blank=True,
+        help_text=_("The merchant name, as enriched by Plaid from the name field."),
+    )
+    merchant_entity_id = models.TextField(
+        blank=True,
+        help_text=_("A unique, stable, Plaid-generated ID that maps to the merchant."),
+    )
+    pending = models.BooleanField(help_text=_("When true, identifies the transaction as pending or unsettled."))
+    account_owner = models.TextField(
+        blank=True,
+        help_text=_(
+            "The name of the account owner. This field is not typically populated and only relevant when dealing with sub-accounts."
+        ),
+    )
+    logo_url = models.URLField(
+        blank=True,
+        null=True,
+        help_text=_(
+            "The URL of a logo associated with this transaction, if available. The logo will always be 100×100 pixel PNG file."
+        ),
+    )
+    website = models.TextField(
+        blank=True,
+        help_text=_("The website associated with this transaction, if available."),
+    )
+    date = models.DateField(
+        help_text=_(
+            "For pending transactions, the date that the transaction occurred; for posted transactions, the date that the transaction posted."
+        )
+    )
+    authorized_date = models.DateField(
+        blank=True,
+        null=True,
+        help_text=_(
+            "The date that the transaction was authorized. For posted transactions, the date field will indicate the posted date, "
+            "but authorized_date will indicate the day the transaction was authorized by the financial institution. "
+            "If presenting transactions to the user in a UI, the authorized_date, when available, is generally preferable "
+            "to use over the date field for posted transactions, as it will generally represent the date the user actually made the transaction."
+        ),
+    )
+    datetime = models.DateTimeField(
+        blank=True,
+        null=True,
+        help_text=_("Date and time when a transaction was posted."),
+    )
+    authorized_datetime = models.DateTimeField(
+        blank=True,
+        null=True,
+        help_text=_(
+            "The date and time that the transaction was authorized. For posted transactions, the datetime field will indicate the posted date, "
+            "but authorized_date will indicate the day the transaction was authorized by the financial institution. "
+            "If presenting transactions to the user in a UI, the authorized_date, when available, is generally preferable "
+            "to use over the date field for posted transactions, as it will generally represent the date the user actually made the transaction."
+        ),
+    )
+    primary_personal_finance_category = models.TextField(
+        blank=True,
+        help_text=_("A high level category that communicates the broad category of the transaction."),
+    )
+    detailed_personal_finance_category = models.TextField(
+        blank=True,
+        help_text=_(
+            "A granular category conveying the transaction's intent. This field can also be used as a unique identifier for the category."
+        ),
+    )
+    confidence_level = models.CharField(
+        max_length=200,
+        blank=True,
+        choices=TRANSACTION_CATEGORY_CONFIDENCE_LEVEL,
+        help_text=_(
+            "A description of how confident we are that the provided categories accurately describe the transaction intent."
+        ),
+    )
+    personal_finance_category_icon_url = models.URLField(
+        blank=True,
+        null=True,
+        help_text=_(
+            "The URL of an icon associated with the primary personal finance category. The icon will always be 100×100 pixel PNG file."
+        ),
+    )
+
+    class Meta:
+        ordering = ("-date",)
+
+    def __str__(self):
+        return f"Transaction {self.transaction_id}, Account {self.account}"
